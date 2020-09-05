b98b433c31756d07bb1cab1f84a33041b7efca10
unknown <jarko.paska@gmail.com>
2020-09-05 19:52:14 +0200

    done up to registration template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
+from app.forms import UserForm
 
 def index(request):
     context_dict = {"some": 'haha'}
     return render(request, 'app/index.html', context=context_dict)
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+    return render(request, 'app/register.html', context={'user_form': user_form, 'registered': registered})
 
 # Create your views here.
