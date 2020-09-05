277917fda966c368a390dcc1fe19c6c21b2f5de0
sarisko <sara.kutkova@gmail.com>
2020-09-05 17:44:29 +0200

    index page

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from app.models import Category
+from app.models import Category, Algorithm
 
 admin.site.register(Category)
+admin.site.register(Algorithm)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
+def index(request):
+    context_dict = {"some": 'haha'}
+    return render(request, 'app/index.html', context=context_dict)
+
 # Create your views here.
